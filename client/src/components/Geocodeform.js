34b1e38375c4798cde52b6dd636871bd8d716e1e
Ian MacDonald <ianmacdonald@Ian-MacDonalds-MacBook-Pro.local>
2018-12-28 11:23:44 -0330

    removed longlat printout

--- client/src/components/Geocodeform.js
+++ client/src/components/Geocodeform.js
@@ -25,22 +25,13 @@ export class Geocodeform extends Component {
     holdingLocation: "St. John's Newfoundland"
   };
 
-  //these two functions handle the logging of keys typed into our entry forms
+  //these two functions handle the logging of keys typed into the entry forms
   handleLocation = e => {
     const newLocation = e.target.value;
     this.setState({
-      holdingLocation: newLocation
+      holdingLocation: newLocation,
+      location: newLocation
     });
-  };
-
-  handleKeyDown = e => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      const newLocation = e.target.value;
-      this.setState({
-        location: newLocation
-      });
-    }
   };
 
   render() {
@@ -50,11 +41,11 @@ export class Geocodeform extends Component {
     return (
       <div>
         <h4 className='info'>
-          Enter your location and press{' '}
-          <span className='secondarycolor'>enter</span> to convert it to
-          long/lat. Then press <span className='secondarycolor'>submit</span> to
-          check the next 10 passes of the ISS . St. John's NL is used as an
-          example. You can double check your lat/long here:
+          Enter your location and press
+          <span className='secondarycolor'> submit</span> to check the next{' '}
+          <span className='secondarycolor'>10 passes of the ISS</span> . St.
+          John's NL is used as an example. Your lat/long will be printed in the
+          console and you can double check it here:
           <a
             href='https://www.latlong.net/'
             target='_blank'
@@ -69,12 +60,9 @@ export class Geocodeform extends Component {
           variables={{ location, holdingLocation }}
           notifyOnNetworkStatusChange
         >
-          {({ loading, error, data }) => {
+          {({ error, data }) => {
             //if (networkStatus === 4) return 'Refetching!';
-            if (loading) return <h4>Loading...</h4>;
             if (error) return `Error!: ${error}`;
-            //console.log(data.geocode.results[0].locations[0].latLng.lat);
-            //console.log(location);
             return (
               <div>
                 <form
@@ -91,13 +79,6 @@ export class Geocodeform extends Component {
                     margin='normal'
                   />
                 </form>
-                <br />
-                <h6 className='latlng'>
-                  Your lat: {data.geocode.results[0].locations[0].latLng.lat}
-                  <br />
-                  Your long: {data.geocode.results[0].locations[0].latLng.lng}
-                </h6>
-                <br />
                 <button
                   className='btn btn-warning'
                   onClick={() => {
